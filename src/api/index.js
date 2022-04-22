d7e5d950da0771ffbbcbb70de1fdb71505554a04
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-04-22 13:02:51 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,6 @@ import mockrequests from './mockAjax';
 export const reqHomeSlide2 = () => requests({ url: '/api', method: 'get' });
 export const reqHomeSlide = () => mockrequests.get('/homeSlide');
 export const reqHomeAd = () => mockrequests.get('/homeAd');
+
+//要給空對象
+export const reqHomeAdPost = (params) => requests({ url: '/api', method: 'post',data:params});
