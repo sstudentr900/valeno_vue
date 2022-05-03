400bb58f0aad6342c4483a1ecbde6dcc037bac53
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-05-03 13:00:23 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ export const productViewReq = () => requestMock.get('/productView');
 export const newsReq = () => requestMock.get('/news');
 export const newsViewReq = () => requestMock.get('/newsView');
 export const beautyReq = () => requestMock.get('/beauty');
-export const storeReq = () => requestMock.get('/store');
+export const storeReq = () => requestMock({ url: '/store', method: 'get' });
 export const contactReq = () => requestMock.get('/contact');
 export const contactOkReq = () => requestMock.get('/contactOk');
 export const memberReq = () => requestMock.get('/member');
@@ -22,6 +22,8 @@ export const checkoutReq = () => requestMock.get('/checkout');
 export const checkoutEnterReq = () => requestMock.get('/checkoutEnter');
 export const checkoutOkReq = () => requestMock.get('/checkoutOk');
 export const faqReq = () => requestMock.get('/faq');
+
+
 
 //要給空對象
 export const reqHomeAdPost = (params) => requests({ url: '/api', method: 'post', data: params });
